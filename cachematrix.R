54d77e034870fa70edc33793aed40abcf3588803
Nirr3 <nankarstad@hotmail.com>
2018-02-17 14:38:32 -0600

    Adding Coursera Homework for Week 2 to Github

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This set of function creates a matrix and the inverse of a matrix
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()){
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+  }
 
-}
 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve 
+## is retrieve the inverse from the cache.n
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  inv <-x$getinverse
+  if (!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <-x$get()
+  inv(matrix(data,...))
+  x$setinverse(inv)
+  inv
 }
